026d0fe58c48223b8a5dbf31c9be64e6d08a7315
Laura Daniela Munoz Contreras <200134672+laura12006@users.noreply.github.com>
2025-08-22 09:46:15 -0500

    Por fin

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Navbar from './Navbar.jsx'
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <div className="p-8 text-center">
+        <h1 className="text-4xl font-bold text-gray-900 mb-4">
+          Bienvenido a mi navbar
+        </h1>
+        <p className="text-gray-600 text-lg">
+          texto ilustrativo
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default App
